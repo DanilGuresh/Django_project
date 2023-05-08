5d374a9f56e94c788a45e62d7adabe104dafa7d0
Danil Guresh <11den110@gmail.com>
2023-05-08 18:44:17 +0300

    Creating a home page

--- learning_log/learning_logs/models.py
+++ learning_log/learning_logs/models.py
@@ -9,3 +9,20 @@ class Topic(models.Model):
     def __str__(self):
         """Returns a string representation of the model"""
         return self.text
+
+
+class Entry(models.Model):
+    """Information, study by the user on the topic"""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """Returns a string representation of the model"""
+        if len(self.text) > 50:
+            return self.text[:50] + '...'
+        else:
+            return self.text[:50]
