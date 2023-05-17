e8176b52bcb8124423265debad01d0c8cfbd5278
Danil Guresh <11den110@gmail.com>
2023-05-17 17:36:43 +0300

    Building pages

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 urlpatterns = [
     # Homepage
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    # Show all topics
+    path('topics/', views.topics, name='topics'),
+    # Page with selected information on a specific topic
+    path(r'topics/(?P<topic_id>\d+)/', views.topic, name='topic')
 ]
--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
+from .models import Topic
 
 
 def index(request):
     """Home page of the Learning Log app"""
     return render(request, 'learning_logs/index.html')
+
+
+def topics(request):
+    """Displays a list of topics"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
+
+
+def topic(request, topic_id):
+    """Displays one topic and all of its posts"""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topics': topics, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
