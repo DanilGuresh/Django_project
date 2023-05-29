88daa16028512d066925f29994765d90e1331783
Danil Guresh <11den110@gmail.com>
2023-05-29 13:28:18 +0300

    Data editing

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -8,5 +8,12 @@ urlpatterns = [
     # Show all topics
     path('topics/', views.topics, name='topics'),
     # Page with selected information on a specific topic
-    path(r'topics/(?P<topic_id>\d+)/', views.topic, name='topic')
+    path(r'topics/(?P<topic_id>\d+)/', views.topic, name='topic'),
+    # Page for adding a new topic
+    path('new_topic/', views.new_topic, name='new_topic'),
+    # Page for adding a new record
+    path(r'new_entry/(?P<topic_id>\d+)/', views.new_entry, name='new_entry'),
+    # Post edit page
+    path(r'edit_entry/(?P<entry_id>\d+)/', views.edit_entry, name='edit_entry'),
+
 ]
